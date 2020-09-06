358e82ac4b4d066a0e887c5383058f742563baeb
Saikat Sarkar <saikat.dakshana15@gmail.com>
2020-09-07 02:32:34 +0530

    Added a endpoint for uploading image to server.

--- server/app.js
+++ server/app.js
@@ -2,36 +2,76 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+const Busboy = require('busboy');
 
 const authRouter = require('./routes/auth')
 const profileRouter = require('./routes/profile')
-const User = require('./models/User')
 
 const PORT = 4000
 
+
+// initialize firbase admin
+const admin = require("firebase-admin");
+const serviceAccount = require("/home/saikat/Projects/serviceAccountKey.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "instagram-24e71.appspot.com"
+});
+const bucket = admin.storage().bucket();
+
+
 // database connection
-mongoose.connect("mongodb+srv://admin:admin@cluster0.kepi8.mongodb.net/<dbname>?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true}).
-then(()=>{
-    console.log("Connected to database")
-}).
-catch((err)=>{
-    console.log(err)
-})
+mongoose.connect("mongodb+srv://admin:admin@cluster0.kepi8.mongodb.net/<dbname>?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).
+    then(() => {
+        console.log("Connected to database")
+    }).
+    catch((err) => {
+        console.log(err)
+    })
 
 // for cathing error after initial connection
 mongoose.connection.on('error', err => {
     console.log(err)
 });
 
+
 // middlewares
 // middleware for parsing application/json data in request
 app.use(express.json());
 app.use(cookieParser())
 
+
+// routers
 app.use('/api/auth', authRouter);
 app.use('/api/profile', profileRouter);
 
 
-app.listen(PORT, function(){
+// An endpoint for uploading image it returns the url
+app.post('/api/uploadImage', function (req, res) {
+    const busboy = new Busboy({ headers: req.headers });
+
+    busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+
+        const blob = bucket.file(filename.replace(/ /g, "_"))
+        const blobStream = blob.createWriteStream({
+            resumable: false
+        })
+        blobStream.on('finish', () => {
+            let publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+            res.send({publicUrl})
+        })
+        .on('error', () => {
+            res.send({error: "Error in uploading"})
+        })
+
+        file.pipe(blobStream)
+    });
+
+    return req.pipe(busboy);
+})
+
+
+// starting a server at port PORT
+app.listen(PORT, function () {
     console.log("Server started at port " + PORT)
 });
